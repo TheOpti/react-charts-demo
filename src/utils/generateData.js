6fc072ca6e2243c97f1efb4c134ba2ed17cce98b
TheOpti <wieckowski.piotr.93@gmail.com>
2019-02-18 13:13:04 +0100

    Fix method for generating random data

--- src/utils/generateData.js
+++ src/utils/generateData.js
@@ -3,11 +3,14 @@ function generateData() {
   const data = [];
 
   for (let i = 0; i < 350; i++) {
+    const min = generateRandomNumberBetweenRange(15, 35);
+    const max = generateRandomNumberBetweenRange(100, 135);
+
     data.push([
       new Date(2019, 1, i + 1),
-      generateRandomNumberBetweenRange(75, 90),
-      generateRandomNumberBetweenRange(25, 45),
-      generateRandomNumberBetweenRange(15, 32),
+      min,
+      ( min + max ) / 2,
+      max,
       155,
     ]);
   }
@@ -16,7 +19,7 @@ function generateData() {
 }
 
 function generateRandomNumberBetweenRange(min, max) {
-  return Math.floor(Math.random() * max) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 export default generateData;
